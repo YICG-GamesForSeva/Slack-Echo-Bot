4542b91b97131253865dfa98a3a71a47bbfddcd4
pkcodes92 <pkrish19@outlook.com>
2020-12-04 23:50:18 -0500

    Ensuring that the right changes are being made

---
+++ Source/YICG.Apps.Slack.Echo/YICG.Apps.Slack.Echo/SlackAdapterWithErrorHandler.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="SlackAdapterWithErrorHandler.cs" company="Young Indian Culture Group Inc">
+// Copyright (c) Young Indian Culture Group Inc. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Slack.Echo
+{
+    using Microsoft.Bot.Builder.Adapters.Slack;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// This is the Slack Adapter with error handling implementation.
+    /// </summary>
+    public class SlackAdapterWithErrorHandler : SlackAdapter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SlackAdapterWithErrorHandler"/> class.
+        /// </summary>
+        /// <param name="configuration">Application key value settings.</param>
+        /// <param name="adapterOptions">Slack adapter options.</param>
+        /// <param name="logger">Logging mechanism.</param>
+        public SlackAdapterWithErrorHandler(IConfiguration configuration, SlackAdapterOptions adapterOptions, ILogger<SlackAdapter> logger)
+            : base(configuration, adapterOptions, logger)
+        {
+
+        }
+    }
+}
