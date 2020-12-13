400532b6aebd943093d68e93d4eeef34dd21aa17
Pranav Krishnamurthy <74801920+pkcodes92@users.noreply.github.com>
2020-12-12 20:21:56 -0500

    Slack Implementation (#3)
    * Ensuring that the right changes are being made
    
    * Having the necessary things done for the slack implementation to be done correctly
    
    * Making sure to have the slash commands in progress
    
    Having all of the setup done to be able to work properly
    
    * Having the EchoBot properly wired, and will have the necessary testing done
    
    * Making sure to clear all of the credentials from the appsettings.json file

--- Source/YICG.Apps.Slack.Echo/YICG.Apps.Slack.Echo/Bots/EchoBot.cs
+++ Source/YICG.Apps.Slack.Echo/YICG.Apps.Slack.Echo/Bots/EchoBot.cs
@@ -6,10 +6,16 @@ namespace YICG.Apps.Slack.Echo.Bots
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Builder.Adapters.Slack.Model;
     using Microsoft.Bot.Schema;
+    using Newtonsoft.Json;
+    using SlackAPI;
+    using Attachment = Microsoft.Bot.Schema.Attachment;
 
     /// <summary>
     /// This is the echo bot.
@@ -30,7 +36,7 @@ namespace YICG.Apps.Slack.Echo.Bots
             }
 
             var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken).ConfigureAwait(false);
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText), cancellationToken).ConfigureAwait(false);
         }
 
         /// <summary>
@@ -60,6 +66,62 @@ namespace YICG.Apps.Slack.Echo.Bots
                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken).ConfigureAwait(false);
                 }
             }
+        }
+
+        /// <summary>
+        /// OnEventActivityAsync method returns an async Task.
+        /// </summary>
+        /// <param name="turnContext">The current turn/execution flow; turnContext of ITurnContext{T}, where T is an IActivity.</param>
+        /// <param name="cancellationToken">The cancellation token propagates notifications that the operation(s) should be canceled.</param>
+        /// <returns>A <see cref="Task{TResult}"/>; a unit of execution representing the result of the asynchronous operation.</returns>
+        protected override async Task OnEventActivityAsync(ITurnContext<IEventActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext is null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
+
+            if (turnContext.Activity.Name == "Command")
+            {
+                if (turnContext.Activity.Value.ToString() == "/block")
+                {
+                    var interactiveMessage = MessageFactory.Attachment(CreateInteractiveMessage(Directory.GetCurrentDirectory() + @"\Resources\InteractiveMessage.json"));
+                    await turnContext.SendActivityAsync(interactiveMessage, cancellationToken).ConfigureAwait(false);
+                }
+            }
+
+            if (turnContext.Activity.Value is EventType slackEvent)
+            {
+                if (slackEvent.Type == "message")
+                {
+                    if (slackEvent.AdditionalProperties.ContainsKey("subtype") &&
+                        slackEvent.AdditionalProperties["subtype"].ToString() == "file_share")
+                    {
+                        await turnContext.SendActivityAsync(MessageFactory.Text("Echo: I received a file attachment"), cancellationToken).ConfigureAwait(false);
+                    }
+
+                    if (slackEvent.AdditionalProperties.ContainsKey("subtype") &&
+                        slackEvent.AdditionalProperties["subtype"].ToString() == "link_shared")
+                    {
+                        await turnContext.SendActivityAsync(MessageFactory.Text("Echo: I received a link share"), cancellationToken).ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+
+        private static Attachment CreateInteractiveMessage(string filePath)
+        {
+            var interactiveMsgJson = System.IO.File.ReadAllText(filePath);
+            var adaptiveCardAttachment = JsonConvert.DeserializeObject<Block[]>(interactiveMsgJson);
+            var blockList = adaptiveCardAttachment.ToList();
+            var attachment = new Attachment
+            {
+                Content = blockList,
+                ContentType = "application/json",
+                Name = "blocks",
+            };
+
+            return attachment;
         }
     }
 }
---
+++ Source/YICG.Apps.Slack.Echo/YICG.Apps.Slack.Echo/SlackAdapterWithErrorHandler.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="SlackAdapterWithErrorHandler.cs" company="Young Indian Culture Group Inc">
+// Copyright (c) Young Indian Culture Group Inc. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Slack.Echo
+{
+    using Microsoft.Bot.Builder.Adapters.Slack;
+    using Microsoft.Bot.Builder.Integration.AspNet.Core;
+    using Microsoft.Bot.Builder.TraceExtensions;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// This is the Slack Adapter with error handling implementation.
+    /// </summary>
+    public class SlackAdapterWithErrorHandler : SlackAdapter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SlackAdapterWithErrorHandler"/> class.
+        /// </summary>
+        /// <param name="configuration">Application key value settings.</param>
+        /// <param name="logger">Logging mechanism.</param>
+        public SlackAdapterWithErrorHandler(IConfiguration configuration, ILogger<BotFrameworkHttpAdapter> logger)
+            : base(configuration, null, logger)
+        {
+            this.OnTurnError = async (turnContext, exception) =>
+            {
+                // Log any leaked exception from the application.
+                logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
+
+                // Send a message to the user
+                await turnContext.SendActivityAsync("The bot encountered an error or bug.").ConfigureAwait(false);
+                await turnContext.SendActivityAsync("To continue to run this bot, please fix the bot source code.").ConfigureAwait(false);
+
+                // Send a trace activity, which will be displayed in the Bot Framework Emulator
+                await turnContext.TraceActivityAsync("OnTurnError Trace", exception.Message, "https://www.botframework.com/schemas/error", "TurnError").ConfigureAwait(false);
+            };
+        }
+    }
+}
